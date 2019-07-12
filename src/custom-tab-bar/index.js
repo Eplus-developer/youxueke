574fa9988db671635f6179a19dc793ef9e13d88c
ping wu <2983483048@qq.com>
2019-07-12 16:23:39 +0800

    completed the animation of the tab bar, which can now switch smoothly.

--- src/custom-tab-bar/index.js
+++ src/custom-tab-bar/index.js
@@ -1,6 +1,26 @@
+let post_animation = wx.createAnimation({
+  timingFunction: 'ease'
+})
+
+let tab_animation = wx.createAnimation({
+  timingFunction: 'ease'
+})
+
+let pop_animation = wx.createAnimation({
+  timingFunction: 'ease'
+})
+
+let close_animation = wx.createAnimation({
+  timingFunction: 'ease'
+})
+
 Component({
   data: {
     selected: 1,
+    post_animation: {},
+    tab_animation: {},
+    pop_animation: {},
+    close_animation: {},
     icon: [
       '/static/tabs/home.png',
       '/static/images/user.png',
@@ -22,6 +42,22 @@ Component({
       wx.switchTab({
         url: '/pages/home/main',
       })
+    },
+    post: function () {
+      post_animation.scale(0.6).step({duration: 200})
+      post_animation.scale(1.0).step({duration: 100})
+      this.setData({post_animation: post_animation.export()})
+      setTimeout(function () {
+        pop_animation.bottom('0rpx').step({duration: 700})
+        this.setData({pop_animation: pop_animation.export()})
+      }.bind(this), 300)
+    },
+    collapse: function () {
+      close_animation.rotate(180).step({duration: 1000})
+      close_animation.rotate(-180).step({ duration: 1000 })
+      this.setData({close_animation: close_animation.export()})
+      pop_animation.bottom('-250rpx').step({ duration: 1000 })
+      this.setData({ pop_animation: pop_animation.export() })
     }
   }
 });
